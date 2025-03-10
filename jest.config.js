e4beb993325fb71be1da3cb31668536e484065d2
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-03-10 19:19:35 +0200

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,29 +1,8 @@
 export default {
-  testEnvironment: 'node',
-  preset: 'ts-jest/presets/default-esm',
-  extensionsToTreatAsEsm: ['.ts'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-  testMatch: ['**/__tests__/**/*.test.js', '**/__tests__/**/*.test.ts'],
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.d.ts',
-    '!src/**/__tests__/**',
-  ],
-  transformIgnorePatterns: [],
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      useESM: true,
-    }],
-  },
-  testEnvironmentOptions: {
-    globals: {
-      'ts-jest': {
-        useESM: true,
-      },
-    },
-  },
-}; 
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	rootDir: './',
+	roots: [
+		'<rootDir>/src',
+	],
+};
